074abd6f21d17c3c2110b7e98e4cdc1d24cc9b7a
Arthur Leroy <arthur.leroy.pro@gmail.com>
2022-06-02 23:23:39 +0100

    Update Real_data_study.R

--- Real_data_study.R
+++ Real_data_study.R
@@ -1,38 +1,36 @@
 library(tidyverse)
 library(png)
-
+library(MagmaClustR)
 
 #### TRAIN/TEST SPLITING FUNCTIONS ####
 split_train = function(db, ratio_train)
-{ ## db : the database of all observed data
-  ## ratio_train : number between 0 and 1 indicating the proportion of individuals in the training set
-  #
-  ## return : orginal db with the repartition of individuals between the training and testing set
+{
   n_indiv = db$ID%>% unique()
   n_index = (ratio_train * length(n_indiv)) %>% round()
   index_train = sample(n_indiv, n_index, replace = F)
   
-  db %>% mutate(Training = ifelse(ID %in% index_train, 1,0)) %>% return()
+  db %>% 
+    mutate(Training = ifelse(ID %in% index_train, 1,0)) %>%
+    return()
 }
 
 split_times = function(db, prop_test)
-{ ## db : the database of all observed data
-  ## prop_test : number between 0 and 1 indicating the proportion of points we predict on
-  #
-  ## return : orginal db with the repartition of individuals between the training and testing set
+{ 
   db = db %>% rowid_to_column("Row")
-  row_obs = db %>% group_by(ID) %>% top_n(Input, n = - n() * (1 - prop_test)) %>% pull(Row)
+  row_obs = db %>%
+    group_by(ID) %>%
+    top_n(Input, n = - n() * (1 - prop_test)) %>%
+    pull(Row)
   
-  db %>% ungroup() %>% mutate(Observed = ifelse(db$Row %in% row_obs, 1, 0)) %>% dplyr::select(- Row) %>% return()
+  db %>%
+    ungroup() %>%
+    mutate(Observed = ifelse(db$Row %in% row_obs, 1, 0)) %>%
+    dplyr::select(- Row) %>% return()
 }
 
 #### EVALUATION FUNCTIONS ####
 MSE_clust = function(obs, pred)
 {
-  ## obs : the true observed values
-  ## pred : list of probabilities et the parameters of the mixture, coming out from pred_gp_clust()
-  ####
-  ## return : the RMSE given the vector of errors
   input = obs %>% pull(Input)
   value = obs %>% pull(Output)
   floop = function(k)
@@ -58,11 +56,7 @@ loss = function(x, y)
 }
 
 WCIC = function(obs, pred, level)
-{ ## obs : the true observed values
-  ## pred : list of probabilities et the parameters of the mixture, coming out from pred_gp_clust()
-  ## level : confidence level (% of accepted error)
-  ####
-  ## return : the ratio of observed values lying within the mixture of predicted ICs
+{ 
   t = obs %>% pull(Input)
   value = obs %>% pull(Output)
   floop = function(k)
@@ -85,7 +79,7 @@ raw_db_co2 = read_csv('Real_Data_Study/Data/co2-data.csv')
 raw_db_weight = read_csv('Real_Data_Study/Data/db_gusto_weight.csv') 
 ####
 
-#### Weight study ####
+#### WEIGHT STUDY ####
 set.seed(42)
 ## Split by gender and draw the training/testing sets
 db_w = raw_db_weight  %>% 
@@ -102,7 +96,8 @@ db_w_test = db_w %>%
 
 mod_select = select_nb_cluster(data = db_w_train, 
                                fast_approx = F,
-                               grid_nb_cluster = 2:6)
+                               grid_nb_cluster = 1:6, 
+                               cv_threshold = 1e-2)
 #mod = train_magma(db_w_train)
 #saveRDS(mod_select,'Real_Data_Study/Training/train_weight_Hoo_mod_select.rds')
 mod_select = readRDS('Real_Data_Study/Training/train_weight_Hoo_2clusters.rds')
@@ -147,12 +142,6 @@ db_res %>%
 #   theme_classic() 
 # dev.off()
 # 
-# png("weight_plot_pred_bestclust4.png",res=600, height=120, width= 220, units="mm")
-# plot_magmaclust(pred, cluster = clust, data = new_db,
-#               data_train = db_w_test, mean_k = list_mu) +
-#   geom_point(data = new_db_t, aes(x = Input, y = Output), col = 'green')+ 
-#   theme_classic() 
-# dev.off()
 
 #### CO2 STUDY ####
 list_not_country = c('EU-28', 'Europe', 'Europe (excl. EU-27)', 'World',
